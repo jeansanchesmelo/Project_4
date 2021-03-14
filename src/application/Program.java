008414e10c7999813733332b015691085fb116a6
de Melo <Jean.deMelo@sig.biz>
2021-03-14 17:50:53 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,8 @@
 package application;
 
 import java.util.Date;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -8,9 +10,13 @@ public class Program {
 
 	public static void main(String[] args) {
 		
+		//Department
 		Department obj = new Department(1, "Books");
 		System.out.println(obj);
+		
+		//Seller
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		System.out.println(seller);
 	}
 
